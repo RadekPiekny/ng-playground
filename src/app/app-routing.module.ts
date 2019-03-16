348fcc3d8c9221927d834d3d651871d0cf52a73a
Radek <pieknyradek@gmail.com>
2019-03-16 08:58:18 +0100

    image selection component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ImageSelectionComponent } from './image-selection/image-selection.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bla', component: ImageSelectionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/image-selection/image-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-selection',
+  templateUrl: './image-selection.component.html',
+  styleUrls: ['./image-selection.component.css']
+})
+export class ImageSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
