cb8383915eb6e8758ee0b4602e64b5e8fde94f3c
Radek Piekný <pieknyradek@gmail.com>
2020-05-26 00:00:52 +0200

    params to loading

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loading.component.css']
 })
 export class LoadingComponent implements OnInit {
-
+  @Input() params: LoadingComponentParams;
+  completeURL = window.location.href;
   constructor() { }
 
   ngOnInit() {
+    if (!this.params) {
+      let ff: LoadingComponentParams;
+      this.params = {
+        size: 100,
+        innerRingColor: "rgba(53, 39, 204, 0.248)",
+        middleRingColor: "rgba(59, 45, 181, 0.3)",
+        outerRingColor: "rgba(62, 63, 163, 0.3)",
+        text: "loading",
+        textInside: true,
+        textPulse: false
+      }
+    }
+  }
+  getTextClass(): string {
+    let multiClass: string = '';
+    if (this.params.textPulse) {
+      multiClass += ' pulse'
+    }
+    if (this.params.textInside) {
+      multiClass += ' textInside'
+    }
+    return multiClass
   }
 
+}
+
+interface LoadingComponentParams {
+  size: number;
+  innerRingColor: string;
+  middleRingColor: string;
+  outerRingColor: string;
+  text: string;
+  textInside: boolean;
+  textPulse: boolean;
 }
